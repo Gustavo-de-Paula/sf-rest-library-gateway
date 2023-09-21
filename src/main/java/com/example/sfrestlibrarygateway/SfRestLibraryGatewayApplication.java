9e1cfb9af547a3a7f8cd684288587028ecfcb754
Gustavo de Paula <depaula.gustavo@outlook.com>
2023-09-21 10:55:36 -0300

    Initial commit

---
+++ src/main/java/com/example/sfrestlibrarygateway/SfRestLibraryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfrestlibrarygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfRestLibraryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfRestLibraryGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sfrestlibrarygateway/SfRestLibraryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfrestlibrarygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfRestLibraryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
